dd4fcb57b16484ca28abfa0046a5f10e3ea3ee1a
waibaBikash <kavrelibikash@gmail.com>
2024-04-26 20:12:03 +1000

    create sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
-}, {timestamps:ture});
+}, {timestamps:true});
 
 const User = mongoose.model('User',userSchema);
 
