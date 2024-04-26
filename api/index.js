63ce72ea44ae16ae8a9f668b5b29b31c372bc6ce
waibaBikash <kavrelibikash@gmail.com>
2024-04-26 19:31:03 +1000

    sinup created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -13,10 +15,12 @@ console.log(err);
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, ()=>{
 console.log('Server is running on port 3000 !');
 }
 );
 
-app.use("/api./user", userRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test } from '../controler/user.controler.js';
+import { test } from '../controller/user.controller.js'
 
 
 const router =express.Router();
