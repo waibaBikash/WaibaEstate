43045f43eb471b0ebd619ad6ddb2c97efa5e026a
waibaBikash <kavrelibikash@gmail.com>
2024-04-27 20:55:54 +1000

    sign in api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import{ BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SingIn';
-import SingUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -13,7 +13,7 @@ export default function App() {
    <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/sign-in" element={<SignIn/>} />
-    <Route path="/sign-up" element={<SingUp/>} />
+    <Route path="/sign-up" element={<SignUp/>} />
     <Route path="/about" element={<About/>} />
     <Route path="/profile" element={<Profile/>} />
    </Routes>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,11 @@
-import { set } from 'mongoose';
 import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-export default function SingUp() {
+export default function SingIn() {
   const [formData, setFormData]= useState({})
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+
   const handleChange = (e) => {
    setFormData({
     ...formData,
@@ -17,7 +16,7 @@ export default function SingUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch ('/api/auth/signup', {
+      const res = await fetch ('/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -31,8 +30,6 @@ export default function SingUp() {
         return;
       }
       setLoading(false);
-      setError(null);
-      navigate('/sing-in');
     
     } catch (error) {
       setLoading(false);
@@ -43,7 +40,7 @@ export default function SingUp() {
    
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
