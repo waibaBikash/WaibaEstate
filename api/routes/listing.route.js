3bfd7452230c3a794a9af6a1408719cecacda1bc
waibaBikash <kavrelibikash@gmail.com>
2024-05-01 19:54:46 +1000

    complete delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing  } from '../controller/listing.controller.js';
+import { createListing, deleteListing  } from '../controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 
 router.post('/create', verifyToken, createListing );
+router.delete('/delete/:id', verifyToken, deleteListing );
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -122,6 +122,22 @@ export default function Profile() {
             setShowListingsError(true);
           }
          }
+         const handleListingDelete = async() => {
+          try {
+            const res = await fetch(`/api/listing/delete/${listingId}`, {
+              method: 'DELETE',
+            });
+            const data = await res.json();
+            if(data.success === false){
+              console.log(data.message);
+              return;
+            }
+            setUserListings((prev) => 
+            prev.filter((listing) => listing._id !== listingId));
+          } catch (error) {
+            console.log(error.message);
+          }
+         };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -148,8 +164,7 @@ export default function Profile() {
             </span>
           ) : (
             " "
-          )
-          }
+          )  }
         </p>
         <input 
         type="text" placeholder="username" 
@@ -196,12 +211,13 @@ export default function Profile() {
                    <p>{listing.name}</p>
               </Link>
                  <div className='flex flex-col items-center'>
-                   <button className="text-red-700 uppercase">Delete</button>
+                   <button onClick={()=>handlelistingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
                    <button className="text-green-700 uppercase">Edit</button>
                  </div>
             </div>
           ))}  
-          </div>}
+          </div>
+          }
     </div>
   );
 }
