ffb2be654075a4e7840ffa1343d307c4f90291b8
waibaBikash <kavrelibikash@gmail.com>
2024-05-01 22:48:03 +1000

    save change

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -42,7 +42,7 @@ export default function Listing() {
     <Swiper navigation>
       {listing.imageUrls.map((url)=>(
         <SwiperSlide key={url}>
-             <div className="'h-[550px]' style={{background: `url(${url})`}} center no-repeat"></div>
+             <div className="'h-[550px]' style={{background: `url(${url})`}} center no-repeat, backgroundSize: 'cover' "></div>
         </SwiperSlide>
       ))}
     </Swiper>
