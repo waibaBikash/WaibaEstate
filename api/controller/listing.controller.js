d9b1e20e2d74ec4fadf46c11417fb77aa0896f9a
waibaBikash <kavrelibikash@gmail.com>
2024-05-02 21:07:06 +1000

    create search api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -1,3 +1,4 @@
+import { LuSigma } from "react-icons/lu";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -56,4 +57,49 @@ export const getListing = async (req, res, next) => {
  } catch (error) {
   next(error);
  }
+};
+
+
+export const getListings = async(req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if(offer === undefined || offer === 'false'){
+      offer = {$in: [false, true]};
+    }
+    let furnished = req.query.furnished;
+    if(furnished === undefined || furnished === 'false'){
+      furnished = {$in: [false, true]};
+    }
+    let parking = req.query.parking;
+
+    if(parking === undefined || parking  === 'false'){
+      parking = {$in: ['sale', 'rent']};
+    }
+    let type = req.query.type;
+    
+     if(type === undefined || type === 'all'){
+      type = {$in: ['sale', 'rent']};
+     }
+
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+    const order = req.query.order || 'desc';
+    const listings = await Listing.find({
+      name:{$regex: searchTerm, $options: 'i'},
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+    .sort({[sort]: order})
+    .limit(limit)
+    .skip(startIndex)
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
 };
