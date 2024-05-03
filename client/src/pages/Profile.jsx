b7c47ae5ae923a560cf6c6420e1a3335982553af
waibaBikash <kavrelibikash@gmail.com>
2024-05-03 23:01:06 +1000

    fix link profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -213,11 +213,11 @@ export default function Profile() {
                  <div className='flex flex-col items-center'>
                    <button 
                    onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
-                    <LinK to= {`/update-listing/${listing._id}`}> 
+                    <Link to= {`/update-listing/${listing._id}`}> 
                    <button   
                    className="text-green-700 uppercase">
                      Edit</button>
-                    </LinK>
+                    </Link>
                  </div>
             </div>
           ))}  
