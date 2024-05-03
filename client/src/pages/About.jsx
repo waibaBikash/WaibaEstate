181b9bc952ccd70f2f8af123a3614719199849dc
waibaBikash <kavrelibikash@gmail.com>
2024-05-03 22:25:17 +1000

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Waiba Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Waiba Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent peroperties in the most desirble neighborhoods. Our team of experienced agents is dedicated to providing exceptional services and making the buying and selling process as smooth as possible.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep uderstanding of the local market. Whether you are looking to buy, sell or rent a property, we are here to help you every of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of sevices to our clients. We blieve that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a raality for each and evey one of our clients.
+      </p>
+    </div>
+  );
 }
