42e9b86f3aef476238a65d4d218e020dafb5a82e
waibaBikash <kavrelibikash@gmail.com>
2024-06-23 20:55:08 +1000

    commit change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -209,11 +209,9 @@ export default function Profile() {
         className="text-green-700 w-full">Show Listing</button>
 
         <p className="text-red-700 mt-5">{showListingsError ? 'Error showing listings' : ''}</p>
-
           {userListings && userListings.length > 0 &&  (
           <div className="flex flex-col gap-4">
             <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
-
             { userListings.map((listing) => (
             <div 
             key={listing._id} 
@@ -222,12 +220,10 @@ export default function Profile() {
                 <img src="{listing.imageUrls[0]}" alt="listing cover"
                  className="h-16 w-16 object-contain" />
                 </Link>
-          
               <Link 
               className="text-slate-700 font-semibold hover:underline truncate flex-1" to= {`/listing/${listing._id}`}>
                    <p>{listing.name}</p>
               </Link>
-
                  <div className='flex flex-col items-center'>
                    <button 
                    onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">
