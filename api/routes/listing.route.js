e009b44165411d91e6c7357f43081f7e74546035
waibaBikash <kavrelibikash@gmail.com>
2024-06-29 16:24:50 +1000

    commit change

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,7 +5,6 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-
 router.post('/create', verifyToken, createListing );
 router.delete('/delete/:id', verifyToken, deleteListing );
 router.post('/update/:id', verifyToken, updateListing );
