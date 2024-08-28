ca61bd73525425bc5467e97bee2f473dcf036662
waibaBikash <kavrelibikash@gmail.com>
2024-08-28 21:17:58 +1000

    save change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -13,7 +13,9 @@ export default function About() {
           and making the buying and selling process as smooth as possible.
       </p>
       <p className="mb-4 text-slate-700">
-        Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep uderstanding of the local market. Whether you are looking to buy, sell or rent a property, we are here to help you every of the way.
+        Our mission is to help our clients achieve their real estate goals by providing 
+        expert advice, personalized service, and a deep uderstanding of the local market.
+         Whether you are looking to buy, sell or rent a property, we are here to help you every of the way.
       </p>
       <p className="mb-4 text-slate-700">
         Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of sevices to our clients. We blieve that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a raality for each and evey one of our clients.
