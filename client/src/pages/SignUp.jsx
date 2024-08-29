d8b28bf7fa81a0ff8d68e6b49f3cec771c1e62ff
waibaBikash <kavrelibikash@gmail.com>
2024-08-29 22:17:38 +1000

    commit change

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -58,7 +58,8 @@ export default function SignUp() {
          type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}
         />
         <button disabled={loading} 
-        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...': 'Sign Up'}</button>
+        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...': 'Sign Up'}</button>
         <OAuth />
       </form>
         <div className='flex gap-2 mt-5'>
