be642f46938e4a8e8f6ab340239d6a86eec6ed09
waibaBikash <kavrelibikash@gmail.com>
2024-08-29 22:11:48 +1000

    save change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -223,7 +223,8 @@ export default function Profile() {
                  className="h-16 w-16 object-contain" />
                 </Link>
               <Link 
-              className="text-slate-700 font-semibold hover:underline truncate flex-1" to= {`/listing/${listing._id}`}>
+              className="text-slate-700 font-semibold hover:underline truncate flex-1"
+               to= {`/listing/${listing._id}`}>
                    <p>{listing.name}</p>
               </Link>
                  <div className='flex flex-col items-center'>
