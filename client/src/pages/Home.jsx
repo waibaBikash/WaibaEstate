c39d26c5a859d983c6823438ae36a2a8583cadc5
waibaBikash <kavrelibikash@gmail.com>
2024-08-29 22:06:17 +1000

    save change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -109,7 +109,9 @@ export default function Home() {
                   <h2 className='text-2xl font-semibold text-slate-600'>
                     Recent place for rent
                   </h2>
-                  <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+                  <Link
+                   className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent
+                   </Link>
                 </div>
                  <div className="flex flex-wrap gap-4">
                      {rentListings.map((listing) =>(
