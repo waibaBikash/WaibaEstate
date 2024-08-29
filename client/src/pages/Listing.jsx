a9f1da6bf5cf11c8f0716ba911aa59471915f275
waibaBikash <kavrelibikash@gmail.com>
2024-08-29 22:09:29 +1000

    commit change

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -93,7 +93,9 @@ export default function Listing() {
                   )}
               </div>
              <p className='text-slate-800 '>
-              <span className='font-semibold text-black'>Description - </span>
+              <span 
+               className='font-semibold text-black'>Description - 
+               </span>
               {listing.description}
              </p>
              <ul 
