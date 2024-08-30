3d8a7bac85e7c6263ecc245f5f44c45624fb00bc
waibaBikash <kavrelibikash@gmail.com>
2024-08-30 22:50:54 +1000

    changes made

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -143,7 +143,10 @@ export default function CreateListing() {
       <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
            <div className='flex flex-col gap-4 flex-1'>
               <input 
-              type="text"  placeholder='Name' className='border p-3 rounded-lg' id='name' maxLength='62' minLength='10' required
+              type="text"  placeholder='Name' 
+              className='border p-3 rounded-lg'
+               id='name' maxLength='62' 
+               minLength='10' required
               onChange={handleChange}
               value={formData.name}
               />
